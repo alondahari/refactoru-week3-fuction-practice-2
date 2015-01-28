d36da5a48947ca24ea1912251c1ca94f1a826f64
Alon Dahari <climbinghobo@gmail.com>
2015-01-28 14:21:27 -0700

    add totalLetters()

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ var getName = function(obj){
   return obj.name;
 };
 
-console.log(getName({name: 'Luisa', age: 25}));
+console.log('getName: ', getName({name: 'Luisa', age: 25}));
+
+var totalLetters = function(arr){
+  return arr.join('').length;
+};
+
+console.log('totalLetters: ', totalLetters(['javascript', 'is', 'awesome']));
+console.log('totalLetters: ', totalLetters(['what', 'happened', 'to', 'my', 'function']));
